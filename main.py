c7435b85b8d75edb773373ddd791dc49686fbc01
Brian Harrison <brianhpro@gmail.com>
2018-04-19 15:07:59 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#Stanford Computational Journalism API Exercise 1 - Source: Facebook Graph API endpoint
+#written by Brian Harrison - April 19, 2018 - Python 3.6
+
+import requests
+import json
+
+data_url = "http://2015.compjour.org/files/code/json-examples/graph.facebook-whitehouse.json"
+# fetch the data file
+response = requests.get(data_url)
+text = response.text
+# parse the data
+data = json.loads(text)
+# view the data
+print("DATA:\n",data,"\n")
+print('')
+print('SELECTING API ELEMENTS')
+print('A.',data['checkins'])
+print('B.',data['likes'])
+print('C.',data['location']['longitude'])
+print('D.',data['category_list'][-1]['name'])
