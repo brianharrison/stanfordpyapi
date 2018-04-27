852f16c3a17ce7326ce81b993353ac6a327b6e5e
Brian Harrison <brianhpro@gmail.com>
2018-04-26 23:43:02 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+data_url = "http://2015.compjour.org/files/code/json-examples/spotify-related-to-beyonce.json"
+# fetch the data file
+response = requests.get(data_url)
+text = response.text
+# parse the data
+data = json.loads(text)
+related = data['artists']
+# view the data
+print("DATA:\n",data,"\n")
+print("A.",len(related))
+print("B.",related[4]['name'])
+print("C.",related[11]['followers']['total'])
+print("D.",','.join(list(related[0]['genres'])))
+print("E.",related[-1]['images'][0]['url'])
+print("")
+# loop through Beyonce related artists
+artists = []
+for artist in related:
+  if 'pop' in artist['genres']:
+    artists.append(artist['name'])
+for number,artist in enumerate(artists,1):
+  print(str(number)+".",artist)
